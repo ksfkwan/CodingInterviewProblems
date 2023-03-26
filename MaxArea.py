c3d4953fe4ab14b132891f96d488d382aa79c275
Matthew Kwan <88407866+ksfkwan@users.noreply.github.com>
2023-03-26 13:31:37 +0100

    Update MaxArea.py

--- MaxArea.py
+++ MaxArea.py
@@ -12,7 +12,7 @@ class Solution:
         '''
         result = 0
         left, right = 0, len(heights) - 1
-        while left < right:
+        while left != right:
             result = max(result, min(heights[left], heights[right]) * (right - left))
             if heights[left] <= heights[right]:
                 left += 1 # \min(h_{0}, h_{n-1}), \ldots, \min(h_{n-2}, h_{n-1}) \leq \min(h_{0}, h_{n-1})
