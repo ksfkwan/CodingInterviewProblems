7c11cf65dbfc8c6d39225f78e81f6f47d3344701
Matthew Kwan <88407866+ksfkwan@users.noreply.github.com>
2023-04-06 17:47:53 +0100

    Add files via upload

---
+++ MaxProfit.py
@@ -0,0 +1,23 @@
+'''
+Return only the maximum profit given a non-empty array of non-negative prices of a stock.
+You can buy and sell the same stock on the same day.
+It is possible that there is only one element in the array.
+'''
+
+class Solution:
+    def MaxProfit(self, prices):
+        '''
+        :type prices: List[int]
+        :rtype: int
+        '''
+        left = prices[0]
+        result = 0
+        for right in prices:
+            if right < left:
+                left = right
+            # If p(n) < p(m), eliminate (p(m), p(n)), ..., (p(m), p(n + k)), ..., (p(n - 1), p(n + k)), where n + k + 1 = len(prices).
+            # All the eliminated profits must be less than or equal to those that have been or will be considered.
+            result = max(result, right - left)
+        return result
+
+print(Solution().MaxProfit([8, 9, 17, 17, 5, 19, 18, 6, 11, 5, 17, 15, 4, 13, 4, 3, 18, 19, 19, 6]))
