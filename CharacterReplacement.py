657eeb797e2d40e1cd6993d7ad3fa33895721d25
Matthew Kwan <88407866+ksfkwan@users.noreply.github.com>
2023-04-16 16:01:10 +0100

    Update CharacterReplacement.py

--- CharacterReplacement.py
+++ CharacterReplacement.py
@@ -12,7 +12,7 @@ class Solution:
         frequency = 0
         left = 0
         result = 0
-        for right in range(len(s)): # The first iteration is trivial.
+        for right in range(len(s)): # The first iteration is trivial, and the next letter if it exists is either one of the most frequent letters in the preceding window or not. 
             dictionary[s[right]] = 1 + dictionary.get(s[right], 0)
             frequency = max(frequency, dictionary[s[right]])
             
