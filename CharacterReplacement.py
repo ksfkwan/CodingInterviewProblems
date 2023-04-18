916831a8e5421e132e817d034084ca9b22d9d3fd
Matthew Kwan <88407866+ksfkwan@users.noreply.github.com>
2023-04-18 12:52:59 +0100

    Update CharacterReplacement.py

--- CharacterReplacement.py
+++ CharacterReplacement.py
@@ -1,6 +1,6 @@
 '''
 You are given a non-empty string consisting only of uppercase letters and a non-negative integer less than or equal to the result of the string. You can choose any character of the string and change it to any other uppercase character at most k times.
-The algo may, depending on k, shift the current (non-empty) window if extending it does not increase the frequency.
+The algo may, depending on k, shift the current (non-empty) window if extending it does not increase the frequency. A slower way would be checking every valid window.
 Note that we are to return the longest length only.
 A substring is valid if its length minus the number of occurences of its most frequent letter(s), i.e. the minimum number of operations it needs, is less than or equal to k.
 '''
@@ -22,7 +22,7 @@ class Solution:
                 dictionary[s[left]] -= 1
                 left += 1
             
-            result = max(result, right - left + 1)
+            result = max(result, right - left + 1) # The line of code is run regardless.
             # print(f'left, right = {left}, {right}\ndictionary, frequency, result = {dictionary}, {frequency}, {result}\n')
         return result
 
