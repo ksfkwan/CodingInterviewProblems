5b35082a6388f7e93ad7f8e4d712c6483419a1a9
Matthew Kwan <88407866+ksfkwan@users.noreply.github.com>
2023-04-21 15:11:22 +0100

    Update CharacterReplacement.py

--- CharacterReplacement.py
+++ CharacterReplacement.py
@@ -27,7 +27,7 @@ class Solution:
         return result
 
 '''
-Try 'ADABACDAAAA'.
+Try ('ADABACDAAAA', 2).
 To have a better understanding of an algorithm in the future, you may want to think about how it is better than another one.
 This algorithm initially searches for the first valid substring with a length of 1, then immediately the first valid substring with a length of 2 and so on.
 The position of the last element of the first valid substring with a length of m + 1 if it exists must be greater than that of the last element of the first valid substring with a length of m.
