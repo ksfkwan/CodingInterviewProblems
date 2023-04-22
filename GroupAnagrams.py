3c68d38302354d48a4c3218051e507911c92070e
Matthew Kwan <88407866+ksfkwan@users.noreply.github.com>
2023-04-22 19:16:12 +0100

    Add files via upload

---
+++ GroupAnagrams.py
@@ -0,0 +1,14 @@
+'''
+You are given a non-empty list of anagrams consisting of lowercase English letters.
+'''
+
+from typing import List
+class Solution:
+    def GroupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d = {}
+        for s in strs:
+            l = [0] * 26
+            for c in s: # The list is modified only if the string is non-empty.
+                l[ord(c) - ord("a")] += 1
+            d.setdefault(tuple(l), []).append(s) # Neither a list nor a dictionary can serve as a dictionary key as they are mutable.
+        return list(d.values())
