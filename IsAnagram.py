b4be32d929b1e851c74b800b579fb7329a5a38ad
Matthew Kwan <88407866+ksfkwan@users.noreply.github.com>
2023-04-24 18:44:26 +0100

    Update IsAnagram.py

--- IsAnagram.py
+++ IsAnagram.py
@@ -10,6 +10,5 @@ class Solution:
         for i in range(len(s)): # range(-1, -10, 1) == range(2, 6, -2) for your information.
             sigma[s[i]] = 1 + sigma.get(s[i], 0)
             tau[t[i]] = 1 + sigma.get(t[i], 0)
-        return sigma == tau # Imagine they are non-empty sets of ordered pairs.
-        # If they are subsets of each other, the two non-empty strings have the same length; if they do not have the same length, their sets are not equal. 
+        return sigma == tau 
 
