a9c84a0ac2a752c9c7aaf174c38d8154f8dbe4b2
Matthew Kwan <88407866+ksfkwan@users.noreply.github.com>
2023-04-24 18:38:34 +0100

    Update IsAnagram.py

--- IsAnagram.py
+++ IsAnagram.py
@@ -6,8 +6,10 @@ class Solution:
     def IsAnagram(self, s: str, t: str) -> bool: # It returns a Boolean object.
         if len(s) != len(t):
             return False
-        sigma, tau = {}, {} # Imagine they are non-empty sets of ordered pairs.
-        for i in range(len(s)):
+        sigma, tau = {}, {}
+        for i in range(len(s)): # range(-1, -10, 1) == range(2, 6, -2) for your information.
             sigma[s[i]] = 1 + sigma.get(s[i], 0)
             tau[t[i]] = 1 + sigma.get(t[i], 0)
-        return sigma == tau # Imagine they are non-empty sets of ordered pairs and it returns True only if they are subsets of each other.
+        return sigma == tau # Imagine they are non-empty sets of ordered pairs.
+        # If they are subsets of each other, the two non-empty strings have the same length; if they do not have the same length, their sets are not equal. 
+
