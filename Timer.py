436ddaccdfc46dda1626101e862fc49a5b476f05
Matthew Kwan <88407866+ksfkwan@users.noreply.github.com>
2024-03-30 20:03:22 +0000

    Create Timer.py

---
+++ Timer.py
@@ -0,0 +1,16 @@
+# def decorator(func):
+#     def wrapper(*args, **kwargs):
+#         return func(*args, **kwargs)
+#     return wrapper
+# def g(a=[]):
+#     a.append(1)
+#     print(a)
+# h = decorator(g)
+# g()
+# h()
+
+import time
+def timer(func):
+    def wrapper(*args, **kwargs):
+    	    
+    return wrapper
